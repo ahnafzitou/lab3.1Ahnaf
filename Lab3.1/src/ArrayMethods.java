7ff5a1c1bfcbf5d3254cea2ed78dc94fccfb2007
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-11-09 13:16:43 -0500

    pushy

--- Lab3.1/src/ArrayMethods.java
+++ Lab3.1/src/ArrayMethods.java
@@ -4,19 +4,26 @@ public class ArrayMethods
 	public static void main(String[] args)
 	{
 		int[] intArray = new int[]{4,2,3,2,3,5};
+		int[] arr1 = new int[] {1,2,3};
+		int[] arr2 = new int[] {4,6,5};
 		/*for(int i = 0; i < intArray.length; i++)
 		{
 			intArray[i] = (int)(Math.random() * 10);
 		}*/
 		printArray(intArray);
 		printArray(removeDuplicates(intArray));
+		printArray(productArray(arr1,arr2));
+
 	}
 	public static int[] removeDuplicates(int[] list)
 	{
+		int inc = 0;
 		int[] copy = copyArray(list);
 		int[] temp = new int[copy.length];
+		
 		for(int i = 0; i < copy.length; i++)
 		{
+			
 			boolean isDupe = false;
 			for (int j = i; j< copy.length; j++)
 			{
@@ -27,17 +34,37 @@ public class ArrayMethods
 			}
 			if(!isDupe)
 			{
-				temp[i] = copy[i];
+				temp[inc] = copy[i];
+				inc++;
 			}
 		}
-		return temp;
+		int[] temp2 = new int[inc];
+		for(int h = 0; h < inc; h++)
+		{
+			temp2[h] = temp[h];
+		}
+		return temp2;
 	}
 	public static int[][] productArray(int[] arr1,int[] arr2)
 	{
-		return null; 
+		int[][]product = new int[arr1.length][arr2.length];
+		for (int i = 0; i < arr1.length; i++)
+		{
+			for(int j = 0; j < arr2.length;j++)
+			{
+				product[i][j] = arr1[i]*arr2[j];
+			}
+		}
+		return product; 
 	}
 	public static int[][] pascalTriangle(int n)
 	{
+		int[][] pascArr = new int[n][n];
+		for (int i = 0; i < n; i++)
+		{
+			int[] tempArr = new int[i];
+			pascArr[i] = tempArr;
+		}
 		return null;
 	}
 	public static void printPascalTriangle(int [][] pTriangle)
@@ -61,5 +88,16 @@ public class ArrayMethods
 			System.out.print("[" + arr[i] + "]");
 		}
 		System.out.println();
+	}
+	public static void printArray(int[][] arr)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			for (int j = 0; j < arr[i].length; j++)
+			{
+				System.out.print("[" + arr[i][j] + "]");
+			}
+			System.out.println();
+		}
 	}
 }
